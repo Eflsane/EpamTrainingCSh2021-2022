75a15b2c08a633e75f1a90399aec70cbe6ca92fa
Eflsane <kireevroma01@yandex.ru>
2022-01-08 20:21:02 +0400

    Finished task1.2

---
+++ Task1/Task1_2/Task1_2_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first string:");
+            string originString = Console.ReadLine(); //написать программу, которая
+
+            Console.WriteLine("Enter second string:");
+            string modifierString = Console.ReadLine(); //описание
+            List<char> modifiedChars = new List<char>();
+
+            foreach(char ch in modifierString)
+            {
+                if (!modifiedChars.Contains(ch))
+                    modifiedChars.Add(ch);
+            }
+
+            StringBuilder doubledStringBuilder = new StringBuilder(originString);
+            foreach (Char ch in modifiedChars)
+            {
+                
+                doubledStringBuilder = (doubledStringBuilder.Replace(ch.ToString(), ch.ToString() + ch.ToString()));
+            }
+
+            Console.WriteLine(doubledStringBuilder);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task1/Task1_2/Task1_2_4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sOrigin = "я плохо учил русский язык. забываю начинать предложения с заглавной." +
+                " хорошо, что можно написать программу! но уже слишком поздно. они стуча" +
+                "тся в дверь. " +
+                "может это инопланетяне пытаются выбить дверь? эх, жаль что я раньше не начал изучать программирование.";
+
+            string[] words = sOrigin.Split();
+            string[] sentSeparators = new string[] {".",  "!", "?"};
+
+            int firstWord = -1;
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (firstWord == -1)
+                    firstWord = i;
+                foreach(string separ in sentSeparators)
+                {
+                    if (words[i].Contains(separ))
+                    {
+
+                        words[firstWord] = System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase(words[firstWord]);
+                        Console.WriteLine(words[firstWord]);
+                        firstWord = -1;
+                    }
+                }
+                
+            }
+
+            Console.WriteLine();
+            foreach(string w in words)
+            {
+                Console.Write(w + " ");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
