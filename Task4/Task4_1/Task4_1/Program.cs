87918f241821fde29a2bfc81a7e7e9b6a2cd0676
Eflsane <kireevroma01@yandex.ru>
2022-02-27 20:37:40 +0400

    Task4_1 Completed

---
+++ Task4/Task4_1/Task4_1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task4_1.Helpers;
+using Task4_1.VersionController;
+
+namespace Task4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SerilogHelper.InitializeLogger();
+
+            //args = new string[] { "/r" };
+
+                FileVersionController versionController
+                    = new FileVersionController(@"C:\Users\super\Task4_1\WatchFolder"
+                    , @"C:\Users\super\Task4_1\LogsFolder\Logs.txt");
+            try
+            {
+                if(args.Length == 1)
+                {
+                    if (args[0] == @"/w")
+                    {
+                        versionController.WatchForChanges();
+                        Console.WriteLine("Watch started");
+
+                        Console.ReadLine();
+
+                        versionController.SaveLogs();
+                    }
+                    else if (args[0] == @"/r")
+                    {
+                        Console.WriteLine("Enter a date (in format yyyy.MM.dd HH:mm) files will be revert to:");
+                        string input = Console.ReadLine();
+                        string dateFormat = "yyyy.MM.dd HH:mm";
+                        DateTime dateTimeToReturn = DateTime.ParseExact(input, dateFormat, null);
+
+
+                        versionController.RevertChanges(dateTimeToReturn);
+                        Console.WriteLine($"Changes reverted to date {dateTimeToReturn}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Task4_1 arg " +
+                        "\n/w - starts watcher for files" +
+                        "\n/r - revert changes to date");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Task4_1 arg " +
+                        "\n/w - starts watcher for files" +
+                        "\n/r - revert changes to date");
+                }
+                
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"An error ocurred: {e.Message}");
+
+                Console.ReadLine();
+            }
+            finally
+            {
+                if (args.Length > 0)
+                {
+                    if (args[0] == @"/w")
+                    {
+                        versionController.SaveLogs();
+                    }
+                }                
+            }
+        }
+    }
+}
---
+++ Task4/Task4_1/Task4_1/VersionController/FileVersionReverter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task4_1.VersionController.Changes;
+
+namespace Task4_1.VersionController
+{
+    public class FileVersionReverter
+    {
+        public Action<object> Started = (object sender) => { };
+
+        public void RevertChanges(DateTime dateTimeToReturn, ref ChangesContainer loggedChanges)
+        {
+            Started?.Invoke(this);
+            if (loggedChanges.Changes.Count <= 0)
+                throw new InvalidOperationException("Logs are empty");
+
+            IEnumerable<Change> changesBeforeDate = loggedChanges.Changes.Where(x => x.ChangeDateTime <= dateTimeToReturn)
+                .OrderByDescending(x => x.ChangeDateTime);
+            List<Change> lastChanges = new List<Change>();
+            foreach (Change ch in changesBeforeDate)
+            {
+                if (lastChanges.Any(x => x.File.FullName == ch.File.FullName))
+                    continue;
+
+                lastChanges.Add(ch);
+
+                if (ch.IsDeleted)
+                {
+                    if (!ch.File.Exists)
+                        continue;
+
+                    ch.File.Delete();
+
+                    continue;
+                }
+
+                StreamWriter sr;
+
+                sr = ch.File.CreateText();
+                sr.Write(ch.FileContainment);
+                sr.Close();
+                ch.File.Attributes = ch.Attributes;
+                ch.File.CreationTime = ch.CreationTime;
+                ch.File.LastWriteTime = ch.ChangeDateTime;
+                ch.File.LastAccessTime = ch.ChangeDateTime;
+            }
+
+            IEnumerable<Change> changesAfterDate = loggedChanges.Changes.Where(x => x.ChangeDateTime > dateTimeToReturn)
+                .OrderByDescending(x => x.ChangeDateTime);
+
+            foreach (Change ch in changesAfterDate)
+            {
+                if (lastChanges.Any(x => x.File.FullName == ch.File.FullName))
+                    continue;
+
+                if (!ch.File.Exists)
+                    continue;
+
+                ch.File.Delete();
+            }
+        }
+    }
+}
